daf27dd1a37beec7f92ce5e2000fda032b9444ca
YusriWaiyuesarai <yusrinetwork@gmail.com>
2026-04-19 14:36:12 +0800

    Commit perubahan integrasi Gemini AI

--- public/drawio/js/PostConfig.js
+++
@@ -1,6 +0,0 @@
-/**
- * Copyright (c) 2006-2024, JGraph Holdings Ltd
- * Copyright (c) 2006-2024, draw.io AG
- */
-// null'ing of global vars need to be after init.js
-window.ICONSEARCH_PATH = null;
